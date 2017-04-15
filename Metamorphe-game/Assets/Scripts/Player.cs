329ab891c982cf40d30ecda5caadfe9a5a3c10e4
Benoit DOYEN <Benoit DOYEN>
2017-04-16 00:31:23 +0200

    First push

---
+++ Metamorphe-game/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+        {
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+
+            transform.Find("FirstPersonCharacter").gameObject.SetActive(true);
+        }
+	}
+}
